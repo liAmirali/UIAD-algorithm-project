f4eb59ab1318953a827e0cdf58de5d67e7d68b00
Amirali Lotfi <liamirali.lotfi@gmail.com>
2023-05-26 01:22:26 +0330

    Add mcoloring algorithm

---
+++ src/graph/MColoring.java
@@ -0,0 +1,67 @@
+package graph;
+
+import graph.vertex.Vertex;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class MColoring {
+    private final Graph<?, ?> graph;
+    final private int n;
+    final private ArrayList<int[]> allMColorings;
+    private int[] coloring;
+    private int mColors;
+
+    public MColoring(Graph<?, ?> graph) {
+        this.graph = graph;
+        this.allMColorings = new ArrayList<>();
+        this.n = graph.numVertices();
+        this.coloring = new int[this.n];
+    }
+
+    private boolean promising(int vertexIndex, int[] coloring) {
+        if (vertexIndex == -1) return true;
+
+        Vertex<?> vertex = graph.vertices().get(vertexIndex);
+
+        boolean isPromising = true;
+        int j;
+
+        for (Vertex<?> v : graph.vertices()) {
+            j = graph.vertices().indexOf(v);
+            if (j < vertexIndex && vertex.getOutgoing().containsKey(v) && coloring[j] == coloring[vertexIndex]) {
+                isPromising = false;
+                break;
+            }
+        }
+
+        return isPromising;
+    }
+
+    private void mColoring(int vertexIndex) {
+        if (promising(vertexIndex, coloring)) {
+            if (vertexIndex == n - 1) {
+                int copy_coloring[] = coloring.clone();
+                allMColorings.add(copy_coloring);
+            } else {
+                for (int color = 0; color < mColors; color++) {
+                    coloring[vertexIndex + 1] = color;
+                    mColoring(vertexIndex + 1);
+                }
+            }
+        }
+    }
+
+    public void getColoring(int m) {
+        this.mColors = m;
+
+        mColoring(-1);
+
+        System.out.println("ALL COLORINGS:");
+        for (int[] allMColoring : allMColorings) {
+            System.out.println(Arrays.toString(allMColoring));
+        }
+    }
+
+}
