7b00770b612e902c2a533a043d326f447af88f9d
Arshia Shafiei <arshiashafiei03@gmail.com>
2023-05-26 06:09:13 +0330

    Add carpet and fix some problems

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -1,23 +1,33 @@
+import java.util.ArrayList;
+
 public class Knapsack {
 
+    public void buyCarpet(int money) {
+        ArrayList<Carpet> carpets = new ArrayList<>(); // must change with carpets from system
 
-    public int knapsack(int capacity, int[] weights, int[] values, int n) {
-        int i, w;
+
+    }
+
+    public int[][] knapsack(int capacity, int n, int[] weights, int[] values) {
         int[][] K = new int[n + 1][capacity + 1];
+        boolean[] inSack = new boolean[n];
 
-        // Build table K[][] in bottom up manner
-        for (i = 0; i <= n; i++) {
-            for (w = 0; w <= capacity; w++) {
+
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= capacity; w++) {
                 if (i == 0 || w == 0)
                     K[i][w] = 0;
                 else if (weights[i - 1] <= w)
-                    K[i][w]
-                            = Math.max(values[i - 1] + K[i - 1][w - weights[i - 1]], K[i - 1][w]);
+                    K[i][w] = Math.max(values[i - 1] + K[i - 1][w - weights[i - 1]], K[i - 1][w]);
                 else
                     K[i][w] = K[i - 1][w];
             }
         }
 
-        return K[n][capacity];
+        return K;
+    }
+
+    public void printKnapsack(int[][] array, int capacity, int n, int[] weights, int[] values) {
+
     }
 }
