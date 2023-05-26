68488b70f183f719da69e1002fb13894fbaee9bd
Arshia Shafiei <arshiashafiei03@gmail.com>
2023-05-26 04:45:14 +0330

    Add Tuple and Implement djikstra algorithm

--- src/PathFinder.java
+++ src/PathFinder.java
@@ -2,9 +2,8 @@ import graph.AdjacencyMapGraph;
 import graph.Graph;
 import graph.vertex.Vertex;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+
 
 public class PathFinder {
     private final AdjacencyMapGraph<Intersection, Integer> graph;
@@ -13,11 +12,46 @@ public class PathFinder {
         this.graph = graph;
     }
 
-    private void dijkstra(Graph graph, int srcIndex, int graphSize) {
-        Set<Vertex> unvisited = new HashSet<Vertex>();
-        double[] dist = new double[graphSize];
-        Arrays.fill(dist, Double.POSITIVE_INFINITY);
-        dist[srcIndex] = 0;
+    private void dijkstra(Graph<Intersection, Integer> graph, Vertex<Intersection> src, int size) {
+//        int[] dist = new int[size];
+        Map<Vertex<Intersection>, Integer> dist = new HashMap<>();
+        int[] prev = new int[size];
+        boolean[] visitedVertex = new boolean[size];
+        PriorityQueue<Tuple<Vertex<Intersection>, Integer>> pq = new PriorityQueue<>(2 * size);
+        pq.offer(new Tuple<>(src, 0));
+        for (int i = 0; i < size; i++) {
+            dist
+        }
+        dist.put(src, 0);
+        while (!pq.isEmpty()) {
+            Tuple<Integer, Integer> min = pq.poll();
+            visitedVertex[min.x] = true;
 
+            // We already found a better path before we got to
+            // processing this node so we can ignore it.
+            if (dist[min.x] < min.y) continue;
+
+            graph.getEdge()
+            // Relax edge by updating minimum cost if applicable.
+            double newDist = dist[edge.from] + edge.cost;
+            if (newDist < dist[edge.to]) {
+                prev[edge.to] = edge.from;
+                dist[edge.to] = newDist;
+                pq.offer(new Node(edge.to, dist[edge.to]));
+            }
+        }
+        for (int i = 0; i < size; i++) {
+
+            // Update the distance between neighbouring vertex and source vertex
+            int u = findMinDistance(dist, visitedVertex);
+            visitedVertex[u] = true;
+
+            // Update all the neighbouring vertex distances
+            for (int v = 0; v < size; v++) {
+                if (!visitedVertex[v] && graph[u][v] != 0 && (dist[u] + graph[u][v] < dist[v])) {
+                    dist[v] = dist[u] + graph[u][v];
+                }
+            }
+        }
     }
 }
