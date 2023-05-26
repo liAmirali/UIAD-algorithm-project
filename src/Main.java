a1a1cf8241cff6e3c81ce6cf2c75442d71dd974c
Amirali Lotfi <liamirali.lotfi@gmail.com>
2023-05-26 17:21:59 +0330

    Add sequence alignment algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import algorithms.SequenceAlignment;
 import exceptions.EdgeAlreadyExistsException;
 import exceptions.NoSuchVertexInGraphException;
 import graph.AdjacencyMapGraph;
@@ -7,20 +8,17 @@ import graph.vertex.Vertex;
 
 public class Main {
     public static void main(String[] args) throws NoSuchVertexInGraphException, EdgeAlreadyExistsException {
-        AdjacencyMapGraph<DefaultVertex, Integer> graph = new AdjacencyMapGraph<>(false);
+        // input strings
+        String gene1 = "GCATGCG";
+        String gene2 = "GATTACA";
 
-        Vertex<DefaultVertex> vertex1 = graph.insertVertex(new DefaultVertex(1));
-        Vertex<DefaultVertex> vertex2 = graph.insertVertex(new DefaultVertex(2));
-        Vertex<DefaultVertex> vertex3 = graph.insertVertex(new DefaultVertex(3));
-        Vertex<DefaultVertex> vertex4 = graph.insertVertex(new DefaultVertex(4));
+        // initialising penalties
+        // of different types
+        int misMatchPenalty = 3;
+        int gapPenalty = 2;
 
-        graph.insertEdge(vertex1,vertex2,12);
-        graph.insertEdge(vertex2,vertex3,23);
-        graph.insertEdge(vertex3,vertex4,34);
-        graph.insertEdge(vertex4,vertex1,41);
-        graph.insertEdge(vertex1,vertex3,13);
-
-        MColoring mColoring = new MColoring(graph);
-        mColoring.getColoring(3);
+        // calling the function to
+        // calculate the result
+        SequenceAlignment.getMinimumPenalty(gene1, gene2, misMatchPenalty, gapPenalty);
     }
 }
